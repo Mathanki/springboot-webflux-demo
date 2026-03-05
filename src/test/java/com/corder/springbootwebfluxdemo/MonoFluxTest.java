6a478c5eaad81b172e1fbda7a4683ec313d51d5f
mathanki <mathanki091suren@gmail.com>
2026-03-04 22:59:51 -0500

    understanding Mono & Flux Internal Workflow

---
+++ src/test/java/com/corder/springbootwebfluxdemo/MonoFluxTest.java
@@ -0,0 +1,42 @@
+package com.corder.springbootwebfluxdemo;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class MonoFluxTest {
+    @Test
+    public void testMono(){
+        Mono<?> monoString = Mono.just("java test").log();
+        monoString.subscribe(System.out::println);
+    }
+
+    @Test
+    public void testMonoOnError(){
+        Mono<?> monoString = Mono.just("java test")
+                .then(Mono.error(new RuntimeException("Exception occured")))
+                .log();
+        monoString.subscribe(System.out::println,(e)->System.out.println(e.getMessage()));
+    }
+
+    @Test
+    public void testFlux(){
+        Flux<String> fluxString = Flux.just("Spring", "Spring Boot", "Hibernate", "microservice")
+                .concatWithValues("AWS")
+                .concatWithValues("cloud")
+                .log();
+
+        fluxString.subscribe(System.out::println);
+    }
+
+    @Test
+    public void testFluxOnError(){
+        Flux<String> fluxString = Flux.just("Spring", "Spring Boot", "Hibernate", "microservice")
+                .concatWithValues("AWS")
+                .concatWith(Flux.error(new RuntimeException("Exception occured in Flux")))
+                .concatWithValues("cloud")
+                .log();
+
+        fluxString.subscribe(System.out::println,(e)->System.out.println(e.getMessage()));
+    }
+}
